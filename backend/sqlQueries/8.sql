a5881a557df33e18e7627c8386e0e1802de3bc06
Andrew Zang <andrewz020529@gmail.com>
2022-10-18 22:42:15 -0400

    fix according to Matthew

--- backend/sqlQueries/8.sql
+++ backend/sqlQueries/8.sql
@@ -1,3 +1,9 @@
-SELECT id, userID, productID, description, status, createdAt, updatedAt, productId 
-FROM `348-project`.tickets AS ticket 
-WHERE ticket.userID = 1;
+UPDATE `348-project`.tickets 
+	SET description= "new description",
+		status= "closed",
+        updatedAt= now() 
+	WHERE id = 3;
+    
+-- Testing
+SELECT * FROM `348-project`.tickets 
+	WHERE id = 3;
