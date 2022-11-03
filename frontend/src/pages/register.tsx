6c2aba3c9ac34e3e59c5c8cc8af96f23e912657d
Zhongqi Yue <z3yue@uwaterloo.ca>
2022-11-02 22:49:25 -0400

    added register page

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -2,12 +2,96 @@ import React, { FC } from 'react';
 import { Link } from 'react-router-dom'
 import { AiOutlineTeam } from "react-icons/ai";
 import { BiUserCheck } from "react-icons/bi";
+import { SlUserFollowing } from "react-icons/sl";
+import { useState, useEffect } from 'react'
+
+
 const Register: FC<any> = () => {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
+  const { name, email, password, password2 } = formData
+
+  const onSubmit = () => {
+
+  }
+  const onChange = (e: any) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
   return (
     <>
-        Register
-    
-    </>
+            <section className='heading'>
+                <h1>
+                    <SlUserFollowing /> Register
+                </h1>
+                <p>Please create an account</p>
+            </section>
+
+            <section className='form'>
+                <form onSubmit={onSubmit}>
+                    <div className='form-group'>
+                        <input
+                            type='text'
+                            className='form-control'
+                            id='name'
+                            name='name'
+                            value={name}
+                            onChange={onChange}
+                            placeholder='Enter your name'
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            type='email'
+                            className='form-control'
+                            id='email'
+                            name='email'
+                            value={email}
+                            onChange={onChange}
+                            placeholder='Enter your email'
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            type='password'
+                            className='form-control'
+                            id='password'
+                            name='password'
+                            value={password}
+                            onChange={onChange}
+                            placeholder='Enter password'
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            type='password'
+                            className='form-control'
+                            id='password2'
+                            name='password2'
+                            value={password2}
+                            onChange={onChange}
+                            placeholder='Confirm password'
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <button className='btn btn-block'>Submit</button>
+                    </div>
+                </form>
+            </section>
+
+        </>
   );
 };
 
