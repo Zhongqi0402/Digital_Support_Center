d6d66c17c75553e46daf5a87637f4d25ad3d69e8
Zhongqi Yue <z3yue@uwaterloo.ca>
2022-11-13 12:16:31 -0500

    change before Sunday

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
@@ -7,6 +6,8 @@ import Login from './pages/login'
 import Register from './pages/register';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import PrivateRoute from './components/PrivateRoute'
+import NewTicket from './pages/NewTicket'
 
 function App() {
   return (
@@ -18,6 +19,12 @@ function App() {
               <Route path='/' element={<Home />} />
               <Route path='/login' element={<Login />} />
               <Route path='/register' element={<Register />} />
+
+              <Route path='/new-ticket' element={<PrivateRoute />}>
+                <Route path='/new-ticket' element={<NewTicket />} />
+              </Route>
+
+      
             </Routes>
           </div>
       </Router>
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import { Link } from 'react-router-dom'
 import { AiOutlineSend } from "react-icons/ai";
 import { useState, useEffect} from 'react'
